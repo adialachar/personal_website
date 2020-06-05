742031dea8d84037956103f14d651c2d1353e00f
Aditya Acharya <aacha002@ucr.edu>
2020-06-05 00:23:10 -0700

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,13 @@ module.exports = {
         icon: config.manifestIcon, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-149336217-1",
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
     
